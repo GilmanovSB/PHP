28b28063a8960541d94f6a62bd9c7903521ae08f
Sergey <gilmanov10@yandex.ru>
2020-09-23 21:03:14 +0300

    write test db connect

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
-print_r('Hello, world');
+class ConnectDB{
+    public static function setConnect(){
+        return new PDO('mysql:host=localhost;dbname=test1','root','',[PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+    }
+}
+$connect = ConnectDB::setConnect();
+$query = $connect->query('SELECT * FROM user');
+$result = $query->fetchAll();
+
+var_dump($result);
