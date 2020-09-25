5f36918bca2d4c4702f64762ebacdfe722bd43d9
Sergey <gilmanov10@yandex.ru>
2020-09-25 06:30:13 +0300

    use Singltone

--- index.php
+++ index.php
@@ -1,11 +1,30 @@
 <?php
+trait Singletone{
+    private static $instance = null;
+    private function __constructor(){}
+    private function __clone (){}
+    private function __wakeup (){}
+
+    public static function getInstance(){
+        if (self::$instance == null){
+            return self::$instance = new self();
+        } else {
+            return self::$instance;
+        }
+    }
+}
+
 class ConnectDB{
-    public static function setConnect(){
+
+    use Singletone;
+ 
+    public function setConnect(){
         return new PDO('mysql:host=localhost;dbname=test1','root','',[PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
     }
 }
-$connect = ConnectDB::setConnect();
-$query = $connect->query('SELECT * FROM user');
+$connect = ConnectDB::getInstance();
+$conn = $connect->setConnect();
+$query = $conn->query('SELECT * FROM user');
 $result = $query->fetchAll();
 
 var_dump($result);
