fae60688186fd2e8def72effe48bfefc7c06ea54
Sergey <gilmanov10@outlook.com>
2020-09-26 21:21:16 +0300

    add new class QueryBilder. Relise method getAll, entryDb

--- index.php
+++ index.php
@@ -21,11 +21,26 @@ class ConnectDB{
         return new PDO('mysql:host=localhost;dbname=test1','root','',[PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
     }
 }
-$connect = ConnectDB::getInstance();
-$conn = $connect->setConnect();
-$query = $conn->query('SELECT * FROM user');
-$result = $query->fetchAll();
 
+
+class QueryBilder extends ConnectDB{
+
+    public function getAll(){
+        $db = $this->setConnect()->query('SELECT * FROM user');
+        return $db->fetchAll();
+    }
+    
+    public function entryBd($table, $name, $email){
+        $db = $this->setConnect()->prepare("INSERT INTO $table (name, email) VALUES (:name, :email)");
+        $db->execute(array(':name'=> $name, ':email'=> $email));
+    }
+}
+
+
+
+$obj = new QueryBilder();
+$result = $obj->getAll();
+$obj->entryBd('user', 'Sergey', 'sergey@example.com');
 var_dump($result);
 
 print_r('test SSH connect with github'); 
